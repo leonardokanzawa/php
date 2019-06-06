b663b7ace118c029a9084edcfad054e8c34b00d4
Leonardo Kanzawa <leonardo@7bi.me>
2019-06-05 22:23:46 -0300

    alterand titulo

--- controle-estoque/header.php
+++ controle-estoque/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Controle de Estoque</title>
+    <title>Controle de Estoque php2</title>
 
 
     <!-- Latest compiled and minified CSS -->
